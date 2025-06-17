e01a2d3b0ca432fa3408d6d647044b060ad853d5
David Morais <d.morais.dev@gmail.com>
2025-06-17 11:41:12 -0300

    Criei a section estilo v2

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ const hamburger = document.querySelector(".hamburger");
 const nav = document.querySelector(".nav")
 
 hamburger.addEventListener("click", () => nav.classList.toggle("active"));
+
+const botoes = document.querySelectorAll('.seta');
+
+botoes.forEach(botao => {
+  botao.addEventListener('click', () => {
+    const carrossel = botao.parentElement.querySelector('.carrossel');
+    const direcao = botao.classList.contains('direita') ? 1 : -1;
+    const scrollQuantidade = carrossel.offsetWidth * 0.8;
+
+    carrossel.scrollBy({
+      left: scrollQuantidade * direcao,
+      behavior: 'smooth'
+    });
+  });
+});
